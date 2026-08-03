48c426e2ee2e51b84360e9ad35104aa6172210f2
apalmaPL <apalma@product-league.com>
2026-08-03 16:37:08 +0200

    Update change-error_android.js

--- www/change-error_android.js
+++ www/change-error_android.js
@@ -1,7 +1,4 @@
-const fs = require('fs'),
-      path = require('path'),
-      cheerio = require('cheerio'),
-      utils = require("./utils");
+const utils = require("./utils");
 
 module.exports = function (context) {
     
@@ -9,19 +6,18 @@ module.exports = function (context) {
 
     const directoryPath = context.opts.projectRoot + '/www'; 
     const android_directoryPath = context.opts.projectRoot + '/platforms/android/app/src/main/assets/www';
-    const ios_directoryPath = context.opts.projectRoot + '/platforms/ios/www';
 
-    const targetFilePath = findFileWithWordSync(directoryPath, 'customError');
+    const targetFilePath = utils.findFileWithWordSync(directoryPath, 'customError');
     console.log('Target file path:', targetFilePath);
-    const sourceFilePath = findFileWithWordSync(directoryPath, '_error.html');
+    const sourceFilePath = utils.findFileWithWordSync(directoryPath, '_error.html');
     console.log('Source file path:', sourceFilePath);
-    const source2FilePath = findFileWithWordSync(android_directoryPath, '_error.html');
+    const source2FilePath = utils.findFileWithWordSync(android_directoryPath, '_error.html');
     console.log('Source2 file path:', source2FilePath);
     
     console.log('start changing the _error.html');
 
-    replaceHtmlContent(sourceFilePath, targetFilePath, selector);
-    replaceHtmlContent(source2FilePath, targetFilePath, selector);
+    utils.replaceHtmlContent(sourceFilePath, targetFilePath, selector);
+    utils.replaceHtmlContent(source2FilePath, targetFilePath, selector);
 
     console.log('end changing the _error.html');
 
