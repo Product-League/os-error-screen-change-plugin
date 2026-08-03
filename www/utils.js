d442b1e1011aa51107c19415f8628abbeb991ff5
apalmaPL <apalma@product-league.com>
2026-08-03 16:34:33 +0200

    Create utils.js

---
+++ www/utils.js
@@ -0,0 +1,73 @@
+const fs = require('fs'),
+      path = require('path'),
+      cheerio = require('cheerio');
+
+
+// Function to read file content synchronously
+const readFileSync = (filePath) => {
+  try {
+    return fs.readFileSync(filePath, 'utf8');
+  } catch (err) {
+    console.error(`Error reading file ${filePath}:`, err);
+    throw err;
+  }
+};
+
+
+// Main function to perform the replacement
+const replaceHtmlContent = (sourceFilePath, targetFilePath, selector) => {
+    try {
+
+      const sourceHtml = readFileSync(sourceFilePath);
+      const targetHtml = readFileSync(targetFilePath);
+  
+      // Load the HTML content using cheerio
+      const $source = cheerio.load(sourceHtml);
+      const $target = cheerio.load(targetHtml);
+  
+      // Remove script tag
+      $source("body script").remove();
+
+      // Find the element and replace it in the source HTML
+      $source(selector).html($target.html());
+      
+      // Write the modified HTML back to the _error.html
+      fs.writeFileSync(sourceFilePath, $source.html());
+      console.log('The HTML content has been replaced and saved as "_error.html"');
+  
+    } catch (err) {
+      console.error('Error:', err);
+    }
+  };
+
+
+// Function to find a file with a name that includes a specific word in a directory
+const findFileWithWordSync = (directoryPath, word) => {
+    try {
+
+      const files = fs.readdirSync(directoryPath);
+  
+      // Find the first file that includes the specific word in its name
+      const matchingFile = files.find(file => file.includes(word));
+  
+      if (matchingFile) {
+        // Return the full path of the matching file
+        return path.join(directoryPath, matchingFile);
+      } else {
+        console.log(`No files found with the word "${word}" in their names.`);
+        return null;
+      }
+    } catch (err) {
+      console.error(`Error reading directory ${directoryPath}:`, err);
+      throw err;
+    }
+  };
+
+
+module.exports = {
+    readFileSync,
+    replaceHtmlContent,
+    findFileWithWordSync
+}
+
+
