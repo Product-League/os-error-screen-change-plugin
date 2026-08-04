12556dbe7455b5609faa0baa9e83c51aa590544c
apalmaPL <apalma@product-league.com>
2026-08-04 11:46:13 +0200

    Update change-error-ios.js

--- www/change-error-ios.js
+++ www/change-error-ios.js
@@ -13,11 +13,17 @@ module.exports = function (context) {
     console.log('Source file path:', sourceFilePath);
     const source2FilePath = utils.findFileWithWordSync(ios_directoryPath, '_error.html');
     console.log('Source2 file path:', source2FilePath);
+    const source3FilePath = utils.findFileWithWordSync(directoryPath, 'error.html');
+    console.log('Source file path:', sourceFilePath);
+    const source4FilePath = utils.findFileWithWordSync(ios_directoryPath, 'error.html');
+    console.log('Source2 file path:', source2FilePath);
     
     console.log('start changing the _error.html');
 
     utils.replaceHtmlContent(sourceFilePath, targetFilePath, selector);
     utils.replaceHtmlContent(source2FilePath, targetFilePath, selector);
+    utils.replaceHtmlContent(source3FilePath, targetFilePath, selector);
+    utils.replaceHtmlContent(source4FilePath, targetFilePath, selector);
 
     console.log('end changing the _error.html');
 
