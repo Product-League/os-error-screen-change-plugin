6b1137d187790c27bd16637e4ff868e3a0ff1bab
apalmaPL <apalma@product-league.com>
2026-08-04 22:54:13 +0200

    Update change-error-ios.js

--- www/change-error-ios.js
+++ www/change-error-ios.js
@@ -1,13 +1,46 @@
-const utils = require("./utils/utils"),
-      path = require('path');
+const path = require('path');
+const fs = require('fs');
+const PREFERENCE_NAME = 'CustomErrorJS';
+const utils = require("../utils/utils");
+
+let ExtendedConfigParser;
+try {
+    ExtendedConfigParser = require('../utils/extendedConfigParser');
+} catch (e) {
+    console.error('Failed to require extendedConfigParser:', e.message);
+    console.error(e.stack);
+    throw e;
+}
 
 module.exports = function (context) {
     
-    const selector = '#error-screen-wrapper';
+  // Get the platform (android or ios)
+    const platform = context.opts.cordova.platforms[0];
+      //Get the preference with the JS file to be executed by the error screen
+    const parser = ExtendedConfigParser.createInstance(context);
+    const preferenceValue = parser.getPreferenceValue(PREFERENCE_NAME, platform);
+    let errorJSContent = null;
 
     const directoryPath = context.opts.projectRoot + '/www'; 
     const ios_directoryPath = context.opts.projectRoot + '/platforms/ios/www';
     const error_jsPath = "/MijnHollandZorgApp/" + path.basename(utils.findFileWithWordSync(directoryPath, 'custom-error'));
+    
+    const errorJSPath = directoryPath + "/" + path.basename(utils.findFileWithWordSync(directoryPath, 'custom-error'));
+    
+    if (preferenceValue) {
+        try {
+            // Decode the base64-encoded value
+            const decodedValue = Buffer.from(preferenceValue, 'base64').toString('utf-8');
+            errorJSContent = decodedValue.trim();
+            fs.writeFileSync(errorJSPath, errorJSContent);
+        } catch (e) {
+            console.log('Invalid base64-encoded value for preference ' + PREFERENCE_NAME);
+            return;
+        }
+    }
+
+      
+    const selector = '#error-screen-wrapper';
 
     const targetFilePath = utils.findFileWithWordSync(directoryPath, 'customError');
     console.log('Target file path:', targetFilePath);
