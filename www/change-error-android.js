53bb96b1effecb240a3281e3d7f84ef32828fbde
apalmaPL <apalma@product-league.com>
2026-08-04 11:42:50 +0200

    Update change-error-android.js

--- www/change-error-android.js
+++ www/change-error-android.js
@@ -13,11 +13,14 @@ module.exports = function (context) {
     console.log('Source file path:', sourceFilePath);
     const source2FilePath = utils.findFileWithWordSync(android_directoryPath, '_error.html');
     console.log('Source2 file path:', source2FilePath);
+    const source3FilePath = utils.findFileWithWordSync(directoryPath, 'error.html');
+    console.log('Source2 file path:', source2FilePath);
     
     console.log('start changing the _error.html');
 
     utils.replaceHtmlContent(sourceFilePath, targetFilePath, selector);
     utils.replaceHtmlContent(source2FilePath, targetFilePath, selector);
+    utils.replaceHtmlContent(source3FilePath, targetFilePath, selector);
 
     console.log('end changing the _error.html');
 }
