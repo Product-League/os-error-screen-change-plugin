75d90a0ee77d8a29e3172a793c7fdcd842c3527a
apalmaPL <apalma@product-league.com>
2026-08-04 22:14:14 +0200

    Update extendedConfigParser.js

--- utils/extendedConfigParser.js
+++ utils/extendedConfigParser.js
@@ -45,6 +45,7 @@ function createClass(context) {
                 if (platformParent) parents.push(platformParent);
             }
             for (const parent of parents) {
+                console.log("badruz - " + parent);
                 const elem = parent.findall(`preference[@name="${name}"]`).pop();
                 if (elem) return elem.attrib.value;
             }
